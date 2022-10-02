9b18ec3c32dcea1ba78e8da07c3f0d8b73b2743c
Aleks Saul <aleks.saul@gmail.com>
2022-10-02 15:08:04 -0400

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # gcpregionalquota
 List Regional Quotas to identify quota difference between regions
+
+```
+NAME:
+   GCP Regional Quota lookup -  
+
+USAGE:
+   gcpregionalquota --projectid project-123 --sourceregion us-east1 --targetregion us-central1
+
+DESCRIPTION:
+   Small CLI tool to compare GCP region quotas
+
+COMMANDS:
+   help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --help, -h            show help (default: false)
+   --projectid value     GCP Project ID (required)
+   --sourceregion value  GCP Source Region (required)
+   --targetregion value  GCP Target Region (required)
+```
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	serviceusage "cloud.google.com/go/serviceusage/apiv1"
+	"github.com/urfave/cli/v2"
+	"google.golang.org/api/iterator"
+	serviceusagepb "google.golang.org/genproto/googleapis/api/serviceusage/v1"
+)
+
+func main() {
+	var projectName string
+	var sourceRegion string
+	var targetRegion string
+
+	app := &cli.App{
+		Name:        "GCP Regional Quota lookup",
+		Usage:       " ",
+		UsageText:   "gcpregionalquota --projectid project-123 --sourceregion us-east1 --targetregion us-central1",
+		Description: "Small CLI tool to compare GCP region quotas",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "projectid",
+				Usage:    "GCP Project ID (required)",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "sourceregion",
+				Usage:    "GCP Source Region (required)",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "targetregion",
+				Usage:    "GCP Target Region (required)",
+				Required: true,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			if cCtx.String("projectid") != "" && cCtx.String("sourceregion") != "" && cCtx.String("targetregion") != "" {
+				projectName = cCtx.String("projectid")
+				sourceRegion = cCtx.String("sourceregion")
+				targetRegion = cCtx.String("targetregion")
+			}
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
+	serviceList, err := ListServices(projectName)
+	if err != nil {
+		fmt.Printf("Error getting services in: %s : %e", projectName, err)
+	}
+
+	for i := 0; i < len(serviceList); i++ {
+		GetService(serviceList[i], sourceRegion, targetRegion)
+	}
+}
+
+func GetService(serviceName string, region string, targetregion string) (err error) {
+	ctx := context.Background()
+	c, err := serviceusage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	req := &serviceusagepb.GetServiceRequest{
+		Name: serviceName,
+	}
+	service, err := c.GetService(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(service.Config.Quota.Limits); i++ {
+		ql := service.Config.Quota.Limits[i]
+		sourceregionquota := ql.Values["DEFAULT/"+region]
+		targetregionquota := ql.Values["DEFAULT/"+targetregion]
+		if sourceregionquota == 0 {
+			return
+
+		}
+
+		if sourceregionquota != targetregionquota {
+			fmt.Printf("Service Name: %v\n", service.Config.Name)
+			fmt.Printf("Service Title: %v\n", service.Config.Title)
+			fmt.Printf("Quota Name: %s\n", ql.Name)
+			fmt.Printf("Quota Description: %s\n", ql.DisplayName)
+			fmt.Printf("Quota Description: %s\n", ql.Description)
+			fmt.Printf("Region %s Quota: %v\n", region, ql.Values["DEFAULT/"+region])
+			fmt.Printf("Region %s Quota: %v\n", targetregion, ql.Values["DEFAULT/"+targetregion])
+		}
+
+	}
+
+	return nil
+}
+
+func ListServices(projectName string) (enabledServices []string, e error) {
+	ctx := context.Background()
+	c, err := serviceusage.NewClient(ctx)
+	if err != nil {
+		return enabledServices, err
+	}
+	defer c.Close()
+
+	req := &serviceusagepb.ListServicesRequest{
+		Parent: "projects/" + projectName,
+		Filter: "state:ENABLED",
+	}
+
+	it := c.ListServices(ctx, req)
+
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return enabledServices, err
+		}
+		enabledServices = append(enabledServices, resp.Name)
+
+	}
+	return enabledServices, nil
+}
